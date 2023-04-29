6e29cd888f56bb89885073193069eb9b934f12fb
garciab23 <garciab4898@connect.durhamtech.edu>
2023-04-29 16:35:54 -0400

    second commit, folder and file creation done

--- draftscript.sh
+++ draftscript.sh
@@ -3,7 +3,7 @@
 # Student NOS 125 SP2023
 
 # This script will create a menu
-clear
+#clear
 #echo "Hello, this script will provide options for folder/file creation/delection"
 
 #echo "#####################################################################"
@@ -31,34 +31,40 @@ clear
 #echo "#####################################################################"
 #echo "              #########################################              "
 
-# Check if directory is empty
-
-        if test -d ~/data && ls -a ~/data; then
-                echo "~/data directory is not empty. Do you want to delete all files and folders? (Y/N)"
-                read user_response
-        if "$user_response" == "Y"; then
-                rm -rf ~/data/*
-        else
-                exit
-        fi
+# Number of folders to create
+read -p "Enter the number of folders to be created: " folder_num
 
 # Create folders
-
-#        mkdir -p ~/data/groups/sales
-#        mkdir -p ~/data/groups/marketing
-#        mkdir -p ~/data/public
-#        mkdir -p ~/data/users
-#        mkdir -p ~/data/groups/dev
-#        mkdir -p ~/data/groups/acct
-#        mkdir -p ~/data/public/files
-
+for n in $(seq 1 $folder_num); do
+        mkdir -p ~/data/folder$n
+done
 
 # Number of files to create
 read -p "Enter the number of files to be created: " file_num
-#Create files
-for "(i=1; i<=$file_num; i++)"; do
-dd if=/dev/zero of=~/data/file$i.txt bs=1M count=10
-done
-fi
+
+# Location of files to create
+ls -a ~/data
+echo
+read -p "Enter the location of where to create the files: " file_loc
+
+# Checking for existing files and creating files
+if [ -z "$(ls -A ~/data/$file_loc)" ]; then
+        echo "Folder is empty"
+        for i in $(seq 1 $file_num); do
+                dd if=/dev/zero of=~/data/$file_loc/file$i.txt bs=1M count=10
+        done
+else
+        read -p "Folder is not empty, would you like to overwrite?: [Y/N]" user_response
+        case $user_response in
+        [yY])
+                sudo rm -rf ~/data/$file_loc/file$*.txt
+                for i in $(seq 1 $file_num); do
+                        dd if=/dev/zero of=~/data/$file_loc/file$i.txt bs=1M count=10
+                done
+                ;;
+        [nN])
+                exit
+                ;;
+        esac
 fi
 echo "Files and folders created successfully."
